598eb6eb840b68c5c5870f4ebfcab74c552c87f5
arhes22 <joelngnitedem5@gmail.com>
2023-01-26 12:36:25 +0100

    completed commit

---
+++ resources/views/localisation/localisation.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>
+retrouver nos agences en toute sereniter avec google map
+</h1>
+
+    <div>
+        <p>
+            nous avons un ensemlble de point de distributions de nos services dans un certains nombres de localiter au cameroun.
+            suivez les divers lien si dessous pour nous retrouver.
+        </p>
+        <span>
+            <h4> DOUALA</h4>
+            
+        </span>
+    </div>
+
+</body>
+</html>
